5dbaebd696c8f39d88bf7117b7125f747769acb4
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-02-18 14:59:03 +0800

    Add saving of stats to JSON

--- Assets/Scripts/UI/Rhythm/EndRhythmScreen.cs
+++ Assets/Scripts/UI/Rhythm/EndRhythmScreen.cs
@@ -12,5 +12,11 @@ public class EndRhythmScreen : Singleton<EndRhythmScreen>
         gameObject.transform.GetChild(0).gameObject.SetActive(true);
         gameObject.transform.GetChild(1).gameObject.SetActive(true);
         gameObject.transform.GetChild(2).gameObject.SetActive(true);
+
+        //ADD the (negative) value of changeInTimeRemaining from ManagementModifier to PlayerData timeRemaining
+        PlayerData.Instance.timeRemaining += GetComponent<ManagementModifier>().changeInTimeRemaining;
+
+        //After enabling children wherein PlayerData values are changed, save new values to JSON
+        PlayerData.Instance.SaveStats();
     }
 }
