5ca3fbe98927e47c02bbbef5ea7b46ac128c937a
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-02-25 19:22:24 +0800

    Add StatsScriptableObjects

---
+++ Assets/Scripts/Management/StatsScriptableObjects.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stats")]
+public class StatsScriptableObjects : ScriptableObject
+{
+    public float mentalHealthChange;
+    public float productivityChange;
+    public float timeChange;
+}
