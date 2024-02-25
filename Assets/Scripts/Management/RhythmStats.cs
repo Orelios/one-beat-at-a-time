b25632695cd5c173ab5a618dc53150a50dda5675
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-02-25 20:09:20 +0800

    Add RhythmStats.cs

---
+++ Assets/Scripts/Management/RhythmStats.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmStats : MonoBehaviour
+{
+    [SerializeField] private StatsScriptableObjects stats;
+}
