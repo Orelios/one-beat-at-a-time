67cf70799a7cc67fd450f9cf550f8c33b30cff59
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-02-26 16:43:46 +0800

    Add close ConfirmationScreen button

--- Assets/Scripts/UI/ConfirmationScreen/ConfirmationScreen.cs
+++ Assets/Scripts/UI/ConfirmationScreen/ConfirmationScreen.cs
@@ -11,4 +11,12 @@ public class ConfirmationScreen : Singleton<ConfirmationScreen>
             gameObject.transform.GetChild(i).gameObject.SetActive(true);
         }
     }
+
+    public void DisableChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
 }
