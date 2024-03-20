f81491e4c0b7612c7180fc3c2eb857dbf276cd0c
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-03-21 04:05:38 +0800

    Add codes and pseudocodes

--- Assets/Scripts/Arrows/PatternManager.cs
+++ Assets/Scripts/Arrows/PatternManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PatternManager : Singleton<PatternManager>
 {
-    [SerializeField] private Sprite[] _pressedArrowSprites;
-    [SerializeField] private Arrows[] _arrowInputEnums;
+    [SerializeField] private Sprite[] _correctArrowSprites; //must be set in inspector UP/DOWN/LEFT/RIGHT
+    [SerializeField] private Sprite[] _incorrectArrowSprites; //must be set in inspector UP/DOWN/LEFT/RIGHT
+    [SerializeField] private Arrows[] _arrowInputEnums; //for pattern comparison later - can delete if not needed
     [SerializeField] private PatternsScriptableObjects[] _patterns;
     private GameObject _patternCurrent, _patternPreview, _patternSliding;
     private int _patternIndex = 0, _arrowIndex = 0;
@@ -33,13 +34,23 @@ public class PatternManager : Singleton<PatternManager>
         if (_patterns[_patternIndex].arrowsPattern[_arrowIndex] == arrow)
         {
             _patternCurrent.transform.GetChild(_arrowIndex).GetComponent<SpriteRenderer>().sprite
-                = _pressedArrowSprites[x];
+                = _correctArrowSprites[x];
             _arrowIndex += 1;
         }
         else
         {
             _arrowIndex = 0;
-            //insert function to reset sprites
+            _patternIndex += 1;
+            _patternCurrent.transform.GetChild(_arrowIndex).GetComponent<SpriteRenderer>().sprite = _incorrectArrowSprites[x]; //change current ARROW to wrong version
+            //pulse the pattern
+            //NextPatternSequence(); //AFTER pulse finishes
+        }
+
+        if (_arrowIndex >= 4)
+        {
+            _arrowIndex = 0;
+            _patternIndex += 1;
+            //NextPatternSequence();
         }
     }
 
@@ -54,5 +65,43 @@ public class PatternManager : Singleton<PatternManager>
             pattern.transform.GetChild(i).GetComponent<SpriteRenderer>().sprite
                 = _patterns[_patternIndex].iconPatterns[i].GetComponent<SpriteRenderer>().sprite;
         }
+    }
+
+    //might not be needed since if incorrect arrow was pressed, proceed to next pattern instead of resetting
+    public void ResetPattern(GameObject pattern)
+    {
+        //ResetPattern(_patternCurrent);
+        //_patternCurrent only
+        //change _correctArrowSprites back to _patterns sprites
+        for (int i = 0; i < _patterns[_patternIndex].iconPatterns.Length; i++)
+        {
+            pattern.transform.GetChild(i).GetComponent<SpriteRenderer>().sprite
+                = _patterns[_patternIndex].iconPatterns[i].GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    public void RemoveSprites(GameObject pattern)
+    {
+        for (int i = 0; i < _patterns[_patternIndex].iconPatterns.Length; i++)
+        {
+            pattern.transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = null;
+        }
+    }
+
+    private IEnumerator Slide()
+    {
+        //_patternSliding only - slide down and inc size
+        //when finished, must change position to original and change back to original size
+        yield return null;
+    }
+
+    public void NextPatternSequence()
+    {
+        //RemoveSprites(_patternCurrent);
+        //StartCoroutine(Slide()); //slide down and inc size
+        //SpawnNextPattern(_patternPreview); //done after coroutine starts so they don't overlap
+        //when _patternPreview reaches _patternCurrent position, _patternPreview remove sprites, move back, change size
+        //SpawnNextPattern(_patternCurrent); //gives illusion that _patternSliding becomes new pattern to follow
+        //SpawnNextPattern(_patternSliding); //this will be behind _patternPreview
     }
 }
