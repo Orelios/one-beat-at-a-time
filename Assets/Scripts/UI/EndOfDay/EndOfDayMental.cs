6697f038287e0a1fcfb12662397e71ca4978c73f
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-03-23 19:01:54 +0800

    Add EndOfDayMental

---
+++ Assets/Scripts/UI/EndOfDay/EndOfDayMental.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndOfDayMental : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    void OnEnable()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        text.text = "Mental Health:" + " = " + PlayerData.Instance.mentalHealth;
+
+    }
+}
