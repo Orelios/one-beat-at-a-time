2cf7ea3889f346e211831d9d3176921e4a590d33
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2024-03-24 23:22:13 +0800

    Update Saturday2 and 3

--- Assets/Scripts/UI/AllotedTime.cs
+++ Assets/Scripts/UI/AllotedTime.cs
@@ -8,33 +8,36 @@ public class AllotedTime : MonoBehaviour
     public GameObject allotedTime;
     private void Awake()
     {
-        allotedTime.transform.GetChild(0).gameObject.SetActive(false);
-        allotedTime.transform.GetChild(1).gameObject.SetActive(false);
-        allotedTime.transform.GetChild(2).gameObject.SetActive(false);
+        allotedTime.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        allotedTime.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        allotedTime.transform.GetChild(2).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
     }
 
     void Update()
     {
         if(PlayerData.Instance.timeslot == 1) 
         {
-            allotedTime.transform.GetChild(0).gameObject.SetActive(true);
+            allotedTime.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            allotedTime.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            allotedTime.transform.GetChild(2).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
         }
         else if(PlayerData.Instance.timeslot == 2) 
         {
-            allotedTime.transform.GetChild(0).gameObject.SetActive(true);
-            allotedTime.transform.GetChild(1).gameObject.SetActive(true);
+            allotedTime.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            allotedTime.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            allotedTime.transform.GetChild(2).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
         }
         else if (PlayerData.Instance.timeslot == 3) 
         {
-            allotedTime.transform.GetChild(0).gameObject.SetActive(true);
-            allotedTime.transform.GetChild(1).gameObject.SetActive(true);
-            allotedTime.transform.GetChild(2).gameObject.SetActive(true);
+            allotedTime.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            allotedTime.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            allotedTime.transform.GetChild(2).GetComponent<Image>().color = new Color(1, 1, 1, 1);
         }
         else
         {
-            allotedTime.transform.GetChild(0).gameObject.SetActive(false);
-            allotedTime.transform.GetChild(1).gameObject.SetActive(false);
-            allotedTime.transform.GetChild(2).gameObject.SetActive(false);
+            allotedTime.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            allotedTime.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            allotedTime.transform.GetChild(2).GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
         }
     }
 }
