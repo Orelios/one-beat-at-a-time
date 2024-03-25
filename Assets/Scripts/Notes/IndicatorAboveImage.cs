e3d3b5aa890fc83bb98defad61e9161ffb83b9df
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2024-03-25 22:41:11 +0800

    Update indicatorAboveImage script

--- Assets/Scripts/Notes/IndicatorAboveImage.cs
+++ Assets/Scripts/Notes/IndicatorAboveImage.cs
@@ -16,21 +16,21 @@ public class IndicatorAboveImage : Singleton<IndicatorAboveImage>
         {
             GetComponent<SpriteRenderer>().sprite = aboveImageIndicator[0];
             SoundEffectManager.Instance.PlayEarlySound();
-            CatSpriteChanger.Instance.CatEarlyOrLate();
+            //CatSpriteChanger.Instance.CatEarlyOrLate();
             //GetComponent<SpriteRenderer>().color = Color.red;
         }
         else if (NoteDetection.Instance.noteInDetector.GetComponent<NoteTiming>().timingValue == 2)
         {
             GetComponent<SpriteRenderer>().sprite = aboveImageIndicator[1];
             SoundEffectManager.Instance.PlayPerfectSound();
-            CatSpriteChanger.Instance.CatPerfect();
+            //CatSpriteChanger.Instance.CatPerfect();
             //GetComponent<SpriteRenderer>().color = Color.green;
         }
         else if (NoteDetection.Instance.noteInDetector.GetComponent<NoteTiming>().timingValue == 3)
         {
             GetComponent<SpriteRenderer>().sprite = aboveImageIndicator[2];
             SoundEffectManager.Instance.PlayLateSound();
-            CatSpriteChanger.Instance.CatEarlyOrLate();
+            //CatSpriteChanger.Instance.CatEarlyOrLate();
             //GetComponent<SpriteRenderer>().color = Color.blue;
         }
         StartCoroutine(RetrunToOriginal());
