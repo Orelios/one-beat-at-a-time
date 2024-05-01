43c7e03bfa1f424ab92f087219189d07ba9c0a75
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-05-01 22:19:40 +0800

    Add new note

--- Assets/Scripts/Notes/NoteSpawner.cs
+++ Assets/Scripts/Notes/NoteSpawner.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class NoteSpawner : MonoBehaviour
 {
-    public GameObject notes; 
+    public GameObject detector;
+    public GameObject notes;
+    private float distance;
+    public float travelTime;
+    private float noteSpeed;
+
+    private void Start()
+    {
+        if (detector == null)
+        {
+            detector = GameObject.Find("/NoteManager/NoteDetector");
+        }
+    }
+
     public void spawnNotes()
     {
-        Instantiate(notes, transform.position, transform.rotation, GameObject.FindGameObjectWithTag("NoteSpawner").transform); 
+        //Distance = Vector3.Distance(Object1.transform.position, Object2.transform.position);
+        distance = Vector3.Distance(detector.transform.position, this.gameObject.transform.position);
+        noteSpeed = distance / travelTime;
+        GameObject noteInstance = Instantiate(notes, transform.position, transform.rotation, this.gameObject.transform);
+        noteInstance.GetComponent<Rigidbody2D>().velocity = new Vector3(-noteSpeed, 0, 0);
+        //old code
+        //Instantiate(notes, transform.position, transform.rotation, GameObject.FindGameObjectWithTag("NoteSpawner").transform); 
         //Debug.Log("Spawned a note"); 
     }
 }
