93d4cf485af91c61286f71f88c9e60b791505072
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-05-19 00:36:50 +0800

    Add defaults

--- Assets/Scripts/Offset/OffsetNoteSpawner.cs
+++ Assets/Scripts/Offset/OffsetNoteSpawner.cs
@@ -7,12 +7,12 @@ public class OffsetNoteSpawner : Singleton<OffsetNoteSpawner>
     public GameObject offsetDetector;
     public GameObject notes;
     public float distance;
+    public float travelTimeFrom1200 = 2;
     public float travelTime;
     public float noteSpeed;
-    public float startXPos;
+    public float defaultXPos;
     public Vector3 startPosition;
-    public float relativeNoteSpeed;
-    public float startSpeed;
+    public float defaultNoteSpeed;
 
     private void OnEnable()
     {
@@ -20,19 +20,36 @@ public class OffsetNoteSpawner : Singleton<OffsetNoteSpawner>
         {
             offsetDetector = GameObject.Find("OffsetNoteDetector");
         }
-        startXPos = this.gameObject.transform.position.x;
-        startPosition = this.gameObject.transform.position;
-        CalculateRelativeSpeed();
-        startSpeed = relativeNoteSpeed;
-        noteSpeed = startSpeed;
+        //TEMPORARY - this is to see and set up defaults later. MUST REMOVE before playtesting
+        //defaultXPos = this.gameObject.transform.localPosition.x;
+        startPosition = new Vector3(defaultXPos, 0, 0);
+            //this.gameObject.transform.localPosition;
+
+        /* //if player has adjusted offset, default will not match start, so position will reflect adjusted offset
+        if (defaultXPos != startPosition.x)
+        {
+            this.gameObject.transform.localPosition = startPosition;
+        }
+        */
+
+        //CalculateDefaultSpeed();
+        noteSpeed = defaultNoteSpeed;
     }
 
-    private void CalculateRelativeSpeed()
+    public void CalculateTravelTime()
     {
-        //relativeNoteSpeed is the speed it takes for the note to travel from startPosition of Spawner to Detector in travelTime
-        //if travelTime is 2, it will take the note 2 seconds to travel from STARTPOSITION (not offset) to detector
-        distance = Vector3.Distance(offsetDetector.transform.position, startPosition);
-        relativeNoteSpeed = distance / travelTime;
+        distance = Vector3.Distance(offsetDetector.transform.position, this.gameObject.transform.position);
+        travelTime = distance / noteSpeed;
+    }
+
+    private void CalculateDefaultSpeed() //no longer used so that note speed is constant for all
+    {
+        //defaultNoteSpeed is the speed it takes for the note to travel from defaultPosition of Spawner to Detector in travelTimeFrom1200
+        //if travelTimeFrom1200 is 2, it will take the note 2 seconds to travel from defaultPosition (not offset) to detector
+        distance = Vector3.Distance(offsetDetector.transform.position, this.gameObject.transform.position);
+        defaultNoteSpeed = distance / travelTimeFrom1200;
+
+        //for reference, using 166.25 as noteSpeed, it would take 2 secs from 1200 localPosition.x
     }
     private void Update()
     {
@@ -42,12 +59,12 @@ public class OffsetNoteSpawner : Singleton<OffsetNoteSpawner>
 
     public void UpdatePosition()
     {
-        this.gameObject.transform.position = new Vector3(startXPos + (DisplayOffsetPosition.Instance.offsetPosValue * 10), this.gameObject.transform.position.y, 0);
+        this.gameObject.transform.localPosition = new Vector3(defaultXPos + (DisplayOffsetPosition.Instance.offsetPosValue * 10), this.gameObject.transform.localPosition.y, 0);
     }
 
     public void UpdateSpeed()
     {
-        noteSpeed = startSpeed + (DisplayOffsetSpeed.Instance.offsetSpeedValue * 10);
+        noteSpeed = defaultNoteSpeed + (DisplayOffsetSpeed.Instance.offsetSpeedValue * 10);
     }
 
     public void SpawnNotes()
