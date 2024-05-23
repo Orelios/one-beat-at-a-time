8f4056c0dca61a466d7a82cdc0299c1de7e04eea
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-05-23 17:40:26 +0800

    Fixed positions and speed

--- Assets/Scripts/Debugging/ShowPosition.cs
+++ Assets/Scripts/Debugging/ShowPosition.cs
@@ -6,16 +6,17 @@ using TMPro;
 public class ShowPosition : MonoBehaviour
 {
     TextMeshProUGUI text;
+    public float multiplier = 1.0f;
 
     void OnEnable()
     {
         text = GetComponent<TextMeshProUGUI>();
-        text.text = "" + this.transform.parent.localPosition;
+        text.text = "" + this.transform.parent.localPosition * multiplier;
 
     }
 
     private void Update()
     {
-        text.text = "" + this.transform.parent.localPosition;
+        text.text = "" + this.transform.parent.localPosition * multiplier;
     }
 }
