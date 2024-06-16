f73642995e6ebeffeca126e4880531446844c7ef
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-06-16 19:08:55 +0800

    Restrict player time to press arrows

--- Assets/Scripts/Breathing.cs
+++ Assets/Scripts/Breathing.cs
@@ -11,13 +11,13 @@ public class Breathing : MonoBehaviour
     public ProgressBar progressBar;
     private float BeatManagerStep = 0.5f;
     [SerializeField]
-    private float breathCount = 4, _leewayCount = 2;
-    private float _breathDuration, _leewayDuration, _inhaleLeewayTimer, _holdLeewayTimer, _countUp1, _countUp2, _countUp3, _countUp4;
-    private bool _inhaling, _exhaling, _holding, _inhaleFull, _holdFull;
+    private float breathCount = 4, _leewayCount = 2, _pulseLeeway; //_pulseLeeway is seconds before and after each pulse that player can press
+    private float _breathDuration, _leewayDuration, _inhaleLeewayTimer, _holdLeewayTimer, _countUp1, _countUp2, _countUp3, _countUp4, _canPressTimer;
+    [SerializeField] private bool _inhaling, _exhaling, _holding, _inhaleFull, _holdFull;
     private Vector2 _startScale = new Vector2(1, 1), _endScale = new Vector2(5, 5);
     private float inhalePercentageCompleted, exhalePercentageCompleted, inhaleCount, exhaleCount, holdCount;
-    private Coroutine exhaleCo, inhaleCo, holdCo, inhaleLeewayCo, holdLeewayCo;
-    private bool upPressed, downPressed;
+    private Coroutine exhaleCo, inhaleCo, holdCo, inhaleLeewayCo, holdLeewayCo, canPressCo;
+    [SerializeField] private bool upPressed, downPressed;
     [SerializeField] private Sprite[] _instructionArrowSprites;
     [SerializeField] private Sprite[] _correctArrowSprites;
     [SerializeField] private Sprite[] _incorrectArrowSprites;
@@ -45,51 +45,59 @@ public class Breathing : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            upPressed = true;
-            if (!downPressed)
-            {
-                CheckInhale();
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            upPressed = false;
-            //StopInhale();
-            if (!downPressed)
-            {
-                SetInhaleUI();
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            downPressed = true;
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            downPressed = false;
-            _holdFull = false;
-            //StopHold();
-            //StopExhale();
-            //ResetState();
-        }
+        DetectArrowPress();
+    }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) && upPressed) // Down arrow pressed while Up arrow is STILL pressed
+    private void DetectArrowPress()
+    {
+        if (Player.Instance.canPress == true)
         {
-            if (!_inhaleFull) // Down arrow is pressed too early
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
+                upPressed = true;
+                if (!downPressed)
+                {
+                    CheckInhale();
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                upPressed = false;
                 //StopInhale();
-                ResetState();
-                return;
+                if (!downPressed)
+                {
+                    SetInhaleUI();
+                }
             }
-            CheckHold();
-        }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                downPressed = true;
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                downPressed = false;
+                _holdFull = false;
+                //StopHold();
+                //StopExhale();
+                //ResetState();
+            }
 
-        if (Input.GetKeyUp(KeyCode.UpArrow) && downPressed)
-        {
-            upPressed = false;
-            CheckExhale();
+            if (Input.GetKeyDown(KeyCode.DownArrow) && upPressed) // Down arrow pressed while Up arrow is STILL pressed
+            {
+                if (!_inhaleFull) // Down arrow is pressed too early
+                {
+                    //StopInhale();
+                    ResetState();
+                    return;
+                }
+                CheckHold();
+            }
+
+            if (Input.GetKeyUp(KeyCode.UpArrow) && downPressed)
+            {
+                upPressed = false;
+                CheckExhale();
+            }
         }
     }
 
@@ -367,5 +375,41 @@ public class Breathing : MonoBehaviour
         ui.transform.GetChild(3).GetComponent<Image>().sprite = _instructionArrowSprites[2];
         ui.transform.GetChild(4).GetComponent<Image>().sprite = _instructionArrowSprites[2];
         ui.transform.GetChild(5).GetComponent<TextMeshProUGUI>().text = "Hold";
+    }
+
+    public IEnumerator CanPress()
+    {
+        _canPressTimer = 0;
+        while (_canPressTimer < _countUp1)
+        {
+            _canPressTimer += Time.deltaTime;
+
+            /* at 80 bpm and with 0.5 step, each pulse is 1.5 seconds away from each other
+             * in the if/else statements below,
+             * if _canPressTimer >= 1.3 {canPress = true;}
+             * if _canPressTimer >= 0.2 && _canPressTimer < 1.3 {canPress = false;} //for a total of 1.1 seconds where canPress = false
+             * if _canPressTimer >= 0 && _canPressTimer < 0.2 {canPress = true;}
+             * meaning, if _pulseLeeway is 0.2 seconds, this means that players have 0.2 seconds before and another 0.2 seconds after a pulse to press
+             * for a total of 0.4 seconds where canPress = true
+            */
+            if (_canPressTimer >= _countUp1 - _pulseLeeway) //_pulseLeeway seconds before the next pulse
+            {
+                Player.Instance.canPress = true;
+            }
+            else if (_canPressTimer >= _pulseLeeway) //_pulseLeeway seconds after the pulse
+            {
+                Player.Instance.canPress = false;
+            }
+            else if (_canPressTimer >= 0f) // as soon as pulse
+            {
+                Player.Instance.canPress = true;
+            }
+            yield return null;
+        }
+    }
+
+    public void CanPressCoroutine()
+    {
+        canPressCo = StartCoroutine(CanPress());
     }
 }
