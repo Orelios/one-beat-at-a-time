5104cea3e16af8481bf3edee7117244581d4a59b
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2024-06-29 06:42:55 +0800

    Update end rhythm values

--- Assets/Scripts/UI/Rhythm/EndRhythmMentalHealth.cs
+++ Assets/Scripts/UI/Rhythm/EndRhythmMentalHealth.cs
@@ -28,7 +28,7 @@ public class EndRhythmMentalHealth : MonoBehaviour
 
     private void ChangeIcons()
     {
-        if (valueToAdd <= -6)
+        if (valueToAdd <= -2)
         {
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(1).gameObject.SetActive(true);
@@ -56,7 +56,7 @@ public class EndRhythmMentalHealth : MonoBehaviour
             transform.GetChild(1).gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
             transform.GetChild(2).gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
         }
-        else if (valueToAdd <= -2)
+        else if (valueToAdd <= -6)
         {
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(1).gameObject.SetActive(true);
