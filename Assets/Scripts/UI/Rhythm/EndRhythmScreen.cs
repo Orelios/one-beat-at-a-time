a3332e935e0a69050488aaade80ea65e828286ca
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2024-07-08 16:51:36 +0800

    Fix phone scene not reducing timeslot
    Add childCount variable to automatically count all children

--- Assets/Scripts/UI/Rhythm/EndRhythmScreen.cs
+++ Assets/Scripts/UI/Rhythm/EndRhythmScreen.cs
@@ -6,6 +6,7 @@ public class EndRhythmScreen : Singleton<EndRhythmScreen>
 {
     public ProgressBar progressBar;
     public float successPercent;
+    private int childCount;
 
     public void StopGame()
     {
@@ -21,13 +22,11 @@ public class EndRhythmScreen : Singleton<EndRhythmScreen>
 
         successPercent = (progressBar.GetProgress() / progressBar.GetMaxProgress()) * 100;
 
-        gameObject.transform.GetChild(0).gameObject.SetActive(true);
-        gameObject.transform.GetChild(1).gameObject.SetActive(true);
-        gameObject.transform.GetChild(2).gameObject.SetActive(true);
-        gameObject.transform.GetChild(3).gameObject.SetActive(true);
-        gameObject.transform.GetChild(4).gameObject.SetActive(true);
-        gameObject.transform.GetChild(5).gameObject.SetActive(true);
-        gameObject.transform.GetChild(6).gameObject.SetActive(true);
+        childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(true);
+        }
 
 
         //ADD the (negative) value of timeChaneg from RhythmStats to PlayerData timeslot
